56164febe37bf2c7569a758f5d4072d8256aa877
zyaaa <zybrave@gmail.com>
2024-12-04 13:40:00 -0800

    Initial Commit

---
+++ Bubble converter/Bubble_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_converterApp.swift
+//  Bubble converter
+//
+//  Created by Ethan on 2024-12-04.
+//
+
+import SwiftUI
+
+@main
+struct Bubble_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
